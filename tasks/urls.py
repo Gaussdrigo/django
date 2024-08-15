52617b74e291c21eb2ff2f2e2c6e5b1cc7c9b4cc
Gaussdrigo <alvarengar49@gmail.com>
2024-08-15 16:40:28 -0400

    CRUD TERMINADO

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import list_tasks
+from . import views
 
 urlpatterns = [
-    path('', list_tasks),
+    path("", views.list_tasks, name="list_tasks"),
+    path("add_task/", views.add_task, name="add_task"),
+    path("delete_task/<int:task_id>/", views.delete_task, name="delete_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 
-# Create your views here.
 def list_tasks(request):
-    return render(request, "list_tasks.html")
+    tasks = Task.objects.all()
+    return render(request, "list_tasks.html", {"tasks": tasks})
+
+
+def add_task(request):
+    if request.method == "POST":
+        task_name = request.POST.get("task_name")
+        task_description = request.POST.get("task_description")
+
+        Task.objects.create(name=task_name, description=task_description)
+        return redirect("list_tasks")
+    return redirect("list_tasks")
+
+
+from django.shortcuts import get_object_or_404
+
+
+def delete_task(request, task_id):
+    if request.method == "POST":
+        task = get_object_or_404(Task, id=task_id)
+        task.delete()
+        return redirect("list_tasks")
+    else:
+        # Opcional: Puedes manejar una redirección o mensaje de error si el método no es POST
+        return redirect("list_tasks")
